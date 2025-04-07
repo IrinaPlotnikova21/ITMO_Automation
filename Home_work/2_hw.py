5d72fff2ce54f509258602f074e5fe03ef1a20a8
Ирина <Irabond.sv@yandex.ru>
2025-04-07 20:02:29 +0300

    Рефакторинг ДЗ №2

--- Home_work/2_hw.py
+++ Home_work/2_hw.py
@@ -1,27 +1,29 @@
 #Задача 1
-def task_1 (a: int, b: float, c: str, d: list, e: bool) -> int:
-    return (task_1())
-a = 2
-b = 3.14
-c = 'Hi'
-d = [1, 2, 3]
-e = (10>9)
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
+def task_1 () -> None:
+    a: int = 2
+    b: float = 3.14
+    c: str = 'Hi'
+    d: list = [1, 2, 3]
+    e: bool = True
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    print(type(d))
+    print(type(e))
 
+task_1()
 
 
 #Задача 2
-def task_2 (l = (1, 2, 3, 5, 8, 13, 21)) -> int:
-    return l[0:3]
+def task_2 () -> None:
+    int_tuple: tuple = (1, 2, 3, 5, 8, 13, 21)
+    print(int_tuple[0:3])
 
-print(task_2())
+task_2()
 
 
 #Задача 3
 def task_3 (number: int) -> int:
     return number**2
+
 print(task_3(5))
