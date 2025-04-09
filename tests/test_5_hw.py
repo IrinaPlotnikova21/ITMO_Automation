d3aa3355bc468e19f391a12238b53a872b313a9a
Ирина <Irabond.sv@yandex.ru>
2025-04-09 15:28:19 +0300

    Выполнено ДЗ №5

---
+++ tests/test_5_hw.py
@@ -0,0 +1,35 @@
+
+# from selenium import webdriver
+#
+# driver = webdriver.Chrome(
+# driver.get('https://www.saucedemo.com/')
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+
+driver = webdriver.Chrome()
+driver.get('https://www.saucedemo.com/')
+
+
+# поиск элемента
+def test_search_user_name():
+    user_name = driver.find_element(By.NAME, "user-name")
+    if user_name is None:
+        print('Элемент не найден')
+    else:
+        print('Элемент найден')
+
+def test_search_password():
+    password = driver.find_element(By.ID, "password")
+    if password is None:
+        print('Элемент не найден')
+    else:
+        print('Элемент найден')
+
+def test_search_button():
+    button = driver.find_element(By.ID, "login-button")
+    if button is None:
+        print('Элемент не найден')
+    else:
+        print('Элемент найден')
