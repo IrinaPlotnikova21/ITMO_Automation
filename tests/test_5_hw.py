acce89053cc1380769d4d28ca0a3f60f7e2b38ff
Ирина <Irabond.sv@yandex.ru>
2025-04-09 15:42:22 +0300

    Рефакторинг ДЗ 5 Объединила в одну функцию

--- tests/test_5_hw.py
+++ tests/test_5_hw.py
@@ -13,23 +13,33 @@ driver.get('https://www.saucedemo.com/')
 
 
 # поиск элемента
-def test_search_user_name():
-    user_name = driver.find_element(By.NAME, "user-name")
-    if user_name is None:
-        print('Элемент не найден')
-    else:
-        print('Элемент найден')
+# def test_search_user_name():
+#     user_name = driver.find_element(By.ID, "user-name")
+#     if user_name is None:
+#         print('Элемент не найден')
+#     else:
+#         print('Элемент найден')
+#
+# def test_search_password():
+#     password = driver.find_element(By.ID, "password")
+#     if password is None:
+#         print('Элемент не найден')
+#     else:
+#         print('Элемент найден')
+#
+# def test_search_button():
+#     button = driver.find_element(By.ID, "login-button")
+#     if button is None:
+#         print('Элемент не найден')
+#     else:
+#         print('Элемент найден')
 
-def test_search_password():
+
+def test_search():
     password = driver.find_element(By.ID, "password")
-    if password is None:
-        print('Элемент не найден')
-    else:
-        print('Элемент найден')
-
-def test_search_button():
+    user_name = driver.find_element(By.ID, "user-name")
     button = driver.find_element(By.ID, "login-button")
-    if button is None:
+    if button and user_name and password is None:
         print('Элемент не найден')
     else:
         print('Элемент найден')
